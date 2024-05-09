fc88db7a3180f95629445664146da519310ffdf1
Zach Bergman <bzach634@gmail.com>
2024-05-09 07:28:55 -0600

    test: Update tests for guitars index page

--- spec/features/guitars/index_spec.rb
+++ spec/features/guitars/index_spec.rb
@@ -2,8 +2,9 @@ require "rails_helper"
 
 describe "Guitars Index Page" do
   before(:each) do
-    @guitar_1 = Guitar.create!(store_id: 1, name: "Fender Stratocaster", used: false, price: 680)
-    @guitar_2 = Guitar.create!(store_id: 1, name: "Ibanez RG470DX", used: false, price: 500)
+    @store_1 = Store.create!(name: "Guitar Center", ongoing_sale: true, annual_revenue: 100000)
+    @guitar_1 = @store_1.guitars.create!(name: "Fender Stratocaster", used: false, price: 680)
+    @guitar_2 = @store_1.guitars.create!(name: "Ibanez RG470DX", used: false, price: 500)
   end
 
   describe "As a Visitor" do
@@ -17,15 +18,13 @@ describe "Guitars Index Page" do
       it "displays the guitars attributes" do
         visit("/guitars")
 
-        expect(page).to have_content(@guitar_1.store_id)
-        expect(page).to have_content(@guitar_1.name)
-        expect(page).to have_content(@guitar_1.used)
-        expect(page).to have_content(@guitar_1.price)
+        expect(page).to have_content("Name: #{@guitar_1.name}")
+        expect(page).to have_content("Used?: #{@guitar_1.used}")
+        expect(page).to have_content("Price: $#{@guitar_1.price}")
 
-        expect(page).to have_content(@guitar_2.store_id)
-        expect(page).to have_content(@guitar_2.name)
-        expect(page).to have_content(@guitar_2.used)
-        expect(page).to have_content(@guitar_2.price)
+        expect(page).to have_content("Name: #{@guitar_2.name}")
+        expect(page).to have_content("Used?: #{@guitar_2.used}")
+        expect(page).to have_content("Price: $#{@guitar_2.price}")
       end
     end
   end
