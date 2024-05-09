3fb73f32bddaee9b0bf8aa338d3e257c2abd9203
Zach Bergman <bzach634@gmail.com>
2024-05-09 12:34:17 -0600

    feat: Add extra guitars

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,6 @@
 # Guitars.destroy
 
 @guitar_1 = @store_1.guitars.create!(name: "Fender Stratocaster", used: false, price: 680)
-@guitar_2 = @store_2.guitars.create!(name: "Ibanez RG470DX", used: false, price: 500)
+@guitar_2 = @store_1.guitars.create!(name: "Ibanez RG470DX", used: true, price: 500)
+@guitar_3 = @store_2.guitars.create!(name: "Epiphone Les Paul", used: false, price: 680)
+@guitar_4 = @store_2.guitars.create!(name: "Schecter Omen", used: true, price: 500)
