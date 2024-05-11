0e52a31a3c64f3203c8d77e8ab598b5a51e81f7a
Zach Bergman <bzach634@gmail.com>
2024-05-11 15:18:24 -0600

    feat: Add update and guitar_params - GuitarsController

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -10,4 +10,15 @@ class GuitarsController < ApplicationController
   def edit
     @guitar = Guitar.find(params[:id])
   end
+
+  def update
+    guitar = Guitar.find(params[:id])
+    guitar.update(guitar_params)
+
+    redirect_to("/guitars/#{guitar.id}")
+  end
+
+  def guitar_params
+    params.permit(:name, :used, :price)
+  end
 end
