c0598ae9c5daba4f9cb338bb46354a603beed869
Zach Bergman <bzach634@gmail.com>
2024-05-11 09:19:32 -0600

    feat: Add functionality for creating a new Store

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,13 +3,20 @@ class StoresController < ApplicationController
     @stores = Store.sort_by_created_at
   end
 
-  def show
-    @store = Store.find(params[:id])
-  end
-
   def new
   end
 
   def create
+    store = Store.create!(store_params)
+
+    redirect_to "/stores"
+  end
+
+  def show
+    @store = Store.find(params[:id])
+  end
+
+  def store_params
+    params.permit(:name, :ongoing_sale, :annual_revenue)
   end
 end
