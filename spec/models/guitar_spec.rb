2ab0de7b7c577165f57ab897e7211c496482c096
Zach Bergman <bzach634@gmail.com>
2024-05-11 15:36:46 -0600

    test: Add .display_used_guitars test

--- spec/models/guitar_spec.rb
+++ spec/models/guitar_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 describe Guitar do
+  before(:each) do
+    @store_1 = Store.create!(name: "Guitar Center", ongoing_sale: true, annual_revenue: 100000)
+    @guitar_1 = @store_1.guitars.create!(name: "Fender Stratocaster", used: true, price: 680)
+    @guitar_2 = @store_1.guitars.create!(name: "Ibanez RG470DX", used: false, price: 500)
+    @guitar_3 = @store_1.guitars.create!(name: "Schecter Omen", used: true, price: 720)
+  end
   # describe "validations" do
   #   it { should validate_presense_of :name }
   #   it { should validate_presense_of :used }
@@ -9,5 +15,11 @@ describe Guitar do
   
   describe "relationship" do
     it {should belong_to :store}
+  end
+
+  describe ".display_used_guitars" do
+    it "only displays used guitars" do
+      expect(Guitar.display_used_guitars).to eq([@guitar_1, @guitar_3])
+    end
   end
 end
