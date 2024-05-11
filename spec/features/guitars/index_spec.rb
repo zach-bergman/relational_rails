d3b77d8def2defd6b52d11e677e8469d9623270a
Zach Bergman <bzach634@gmail.com>
2024-05-11 15:36:02 -0600

    test: Add test to Guitars Index Page - display only used guitars

--- spec/features/guitars/index_spec.rb
+++ spec/features/guitars/index_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe "Guitars Index Page" do
   before(:each) do
     @store_1 = Store.create!(name: "Guitar Center", ongoing_sale: true, annual_revenue: 100000)
-    @guitar_1 = @store_1.guitars.create!(name: "Fender Stratocaster", used: false, price: 680)
+    @guitar_1 = @store_1.guitars.create!(name: "Fender Stratocaster", used: true, price: 680)
     @guitar_2 = @store_1.guitars.create!(name: "Ibanez RG470DX", used: false, price: 500)
   end
 
@@ -25,6 +25,18 @@ describe "Guitars Index Page" do
         expect(page).to have_content("Name: #{@guitar_2.name}")
         expect(page).to have_content("Used?: #{@guitar_2.used}")
         expect(page).to have_content("Price: $#{@guitar_2.price}")
+      end
+
+      it "only displays used guitars - used = true" do
+        visit("/guitars")
+
+        expect(page).to have_content("Name: #{@guitar_1.name}")
+        expect(page).to have_content("Used?: #{@guitar_1.used}")
+        expect(page).to have_content("Price: $#{@guitar_1.price}")
+
+        expect(page).to_not have_content("Name: #{@guitar_2.name}")
+        expect(page).to_not have_content("Used?: #{@guitar_2.used}")
+        expect(page).to_not have_content("Price: $#{@guitar_2.price}")
       end
     end
   end
