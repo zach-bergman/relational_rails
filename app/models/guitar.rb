4c8a29143cc6ac545a4e04cabb0571cb744b13d5
Zach Bergman <bzach634@gmail.com>
2024-05-11 05:15:47 -0600

    feat: Add validations - not working, commented out

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -1,3 +1,7 @@
 class Guitar < ApplicationRecord
+  # validates_presense_of :name
+  # validates_presense_of :used
+  # validates_presense_of :price
+
   belongs_to :store
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,8 @@
 class Store < ApplicationRecord
   has_many :guitars
 
+  # validates :name, presence: true
+
   def self.sort_by_created_at
     order(created_at: :DESC)
   end
